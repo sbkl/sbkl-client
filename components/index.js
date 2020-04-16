8cd13218fd79e37e3a7f6ed1e313a2e3a7c40a9c
Sebastien Koziel <sebastien@sbkl.ltd>
2020-04-16 10:22:05 +0800

    fix components import

--- components/index.js
+++ components/index.js
@@ -1,8 +1,22 @@
 export const components = {
   install(Vue, options) {
-    const files = require.context("./", true, /\.vue$/i);
-    files.keys().forEach((key) => {
-      Vue.component(files(key).default.name, files(key).default);
+    const components = require.context("./", true, /\.vue$/i);
+    components.keys().forEach((key) => {
+      Vue.component(components(key).default.name, components(key).default);
+    });
+    const authComponents = require.context("../auth", true, /\.vue$/i);
+    authComponents.keys().forEach((key) => {
+      Vue.component(
+        authComponents(key).default.name,
+        authComponents(key).default
+      );
+    });
+    const layoutsComponents = require.context("../layouts", true, /\.vue$/i);
+    layoutsComponents.keys().forEach((key) => {
+      Vue.component(
+        layoutsComponents(key).default.name,
+        layoutsComponents(key).default
+      );
     });
   },
 };

cb42946ebb436dea1a8d2fec2cacbc58663e87a2
Sebastien Koziel <sebastien@sbkl.ltd>
2020-05-18 14:21:42 +0800

    updated index page template

--- templates/pages/index.vue
+++ templates/pages/index.vue
@@ -8,10 +8,12 @@ export default {
   name: "Welcome",
   mounted() {
     if (this.$auth.user) {
-      if (this.$auth.user.role === "Admin") {
+      if (this.$auth.user.role === "OSL") {
+        this.$router.push({ name: redirectRoutes.user });
+      } else if (this.$auth.user.role === "Admin") {
         this.$router.push({ name: redirectRoutes.admin });
       } else {
-        this.$router.push({ name: redirectRoutes.user });
+        this.$auth.logout();
       }
     } else {
       this.$router.push({ name: "auth-login" });

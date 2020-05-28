f7a9e8859f645cd4b87d6a820d3d3fdd232aaaae
Sebastien Koziel <sebastien@sbkl.ltd>
2020-05-28 12:26:41 +0800

    imported master changes

--- components/DropdownInput.vue
+++ components/DropdownInput.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-<<<<<<< HEAD
-    <div v-if="show" class="absolute inset-0 z-10" @click.self="close"></div>
-    <div class="relative">
-=======
     <div class="">
->>>>>>> cf40c88a78ea0db474be0fe4898e6ebb48e49e19
       <label
         v-if="label != ''"
         class="block text-xs font-medium leading-5 text-gray-700"
@@ -13,78 +8,11 @@
       />
       <div>
         <div class="rounded-md shadow-sm">
-<<<<<<< HEAD
           <button
             ref="button"
             type="button"
             @click="open"
             class="inline-flex justify-between w-full px-4 py-2 text-xs font-medium leading-5 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 active:bg-gray-50 active:text-gray-800"
-            :class="[
-              {
-                'border-red-500':
-                  formType === failedFormName && errors[errorAttribute]
-              },
-              value === '' ? 'text-gray-400' : 'text-gray-700',
-              disabled
-                ? 'cursor-not-allowed text-gray-500'
-                : 'text-gray-700 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue'
-            ]"
-          >
-            <span>{{ value != "" ? value : placeholder}}</span>
-            <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20">
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </button>
-        </div>
-      </div>
-      <div
-        ref="dropdown"
-        v-show="show"
-        class="absolute z-10 w-full p-2 text-xs text-gray-900 bg-gray-800 rounded shadow-lg"
-      >
-        <div class="relative mb-2 bg-gray-200 rounded">
-          <input
-            ref="search"
-            type="text"
-            class="block w-full form-input sm:text-sm sm:leading-5"
-            :class="[
-              showAdditionalAttributes
-                ? 'bg-gray-800 text-white font-bold cursor-not-allowed'
-                : ''
-            ]"
-            :disabled="showAdditionalAttributes"
-            v-model="search"
-            @keydown.esc="close"
-            @keydown.down="highlightNext"
-            @keydown.up="highlightPrev"
-            @keydown.enter.prevent="
-              filteredOptions.length === 0 && onAddItem
-                ? onSubmitAdd(search)
-                : select(filteredOptions[highlightedIndex])
-            "
-            @keydown.tab.prevent="
-              onAddItem != null && !exists
-                ? focusAddButton()
-                : filteredOptions.length === 0
-                ? null
-                : focusRemoveButtonHighlighted()
-            "
-          />
-          <button
-            v-if="onAddItem && !showAdditionalAttributes"
-            ref="add"
-            class="absolute inset-y-0 right-0 px-2 rounded-r-lg focus:outline-none"
-=======
-          <button
-            ref="button"
-            type="button"
-            @click="open"
-            class="inline-flex justify-between w-full px-4 py-2 text-xs font-medium leading-5 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 active:bg-gray-50 active:text-gray-800"
->>>>>>> cf40c88a78ea0db474be0fe4898e6ebb48e49e19
             :class="[
               {
                 'border-red-500':
@@ -95,31 +23,6 @@
                 : 'text-gray-700 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue',
             ]"
           >
-<<<<<<< HEAD
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-5 h-5 fill-current"
-              viewBox="0 0 512 512"
-            >
-              <path
-                d="M368.5 240H272v-96.5c0-8.8-7.2-16-16-16s-16 7.2-16 16V240h-96.5c-8.8 0-16 7.2-16 16 0 4.4 1.8 8.4 4.7 11.3 2.9 2.9 6.9 4.7 11.3 4.7H240v96.5c0 4.4 1.8 8.4 4.7 11.3 2.9 2.9 6.9 4.7 11.3 4.7 8.8 0 16-7.2 16-16V272h96.5c8.8 0 16-7.2 16-16s-7.2-16-16-16z"
-              />
-            </svg>
-          </button>
-          <button
-            ref="close"
-            v-if="showAdditionalAttributes"
-            class="absolute inset-y-0 right-0 px-2 text-white cursor-pointer focus:outline-none focus:text-teal-300"
-            @click.prevent="showAdditionalAttributes = false"
-            @keydown.enter.prevent="showAdditionalAttributes = false"
-            @keydown.tab.prevent="focusAttributeField(0)"
-            @keydown.esc="close"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-6 h-6 fill-current"
-              viewBox="0 0 512 512"
-=======
             <span
               class="truncate"
               :class="value === '' ? 'text-gray-400' : 'text-gray-700'"
@@ -129,7 +32,6 @@
               class="w-5 h-5 ml-2 -mr-1"
               fill="currentColor"
               viewBox="0 0 20 20"
->>>>>>> cf40c88a78ea0db474be0fe4898e6ebb48e49e19
             >
               <path
                 fill-rule="evenodd"
@@ -139,29 +41,6 @@
             </svg>
           </button>
         </div>
-<<<<<<< HEAD
-        <div class="h-64 overflow-auto">
-          <ul
-            ref="options"
-            v-show="filteredOptions.length && !showAdditionalAttributes"
-            class="mt-2"
-          >
-            <li
-              v-for="(option, index) in filteredOptions"
-              :key="option"
-              class="flex items-center justify-between w-full px-2 py-2 text-left text-white rounded cursor-pointer focus:outline-none"
-              :class="[
-                index === highlightedIndex
-                  ? 'bg-teal-300 text-teal-900 font-semibold hover:bg-teal-400 hover:text-white'
-                  : ' hover:bg-gray-700'
-              ]"
-              @click.self="select(option)"
-            >
-              {{ option }}
-              <span
-                v-if="badge(option)"
-                class="flex items-center justify-center w-6 h-6 font-semibold rounded-full"
-=======
       </div>
       <div
         ref="dropdown"
@@ -241,22 +120,11 @@
                 @keydown.esc="close"
                 @click.prevent="onSubmitAdd(search)"
                 class="relative inline-flex items-center p-2 -ml-px transition duration-150 ease-in-out border border-gray-300 rounded-r-md bg-gray-50"
->>>>>>> cf40c88a78ea0db474be0fe4898e6ebb48e49e19
                 :class="[
                   exists
                     ? 'cursor-not-allowed text-gray-400'
                     : 'text-gray-500 hover:bg-white focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700',
                 ]"
-<<<<<<< HEAD
-              >{{ badge(option) }}</span>
-              <button
-                v-if="onRemoveItem && !badge(option)"
-                ref="remove"
-                class="mr-1 focus:text-red-400 hover:text-red-400 focus:outline-none"
-                @keydown.tab.prevent="focusSearchInput"
-                @keydown.enter.prevent="onSubmitRemove(option)"
-                @click.prevent="onSubmitRemove(option)"
-=======
               >
                 <svg
                   class="w-5 h-5"
@@ -276,16 +144,11 @@
                 @click.prevent="showAdditionalAttributes = false"
                 @keydown.enter.prevent="showAdditionalAttributes = false"
                 @keydown.tab.prevent="focusAttributeField(0)"
->>>>>>> cf40c88a78ea0db474be0fe4898e6ebb48e49e19
                 @keydown.esc="close"
                 class="relative inline-flex items-center p-2 -ml-px text-gray-500 transition duration-150 ease-in-out border border-gray-300 rounded-r-md bg-gray-50 hover:bg-white focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700"
               >
                 <svg
-<<<<<<< HEAD
-                  class="w-4 h-4"
-=======
                   class="w-5 h-5"
->>>>>>> cf40c88a78ea0db474be0fe4898e6ebb48e49e19
                   fill="none"
                   stroke-linecap="round"
                   stroke-linejoin="round"
@@ -293,54 +156,6 @@
                   stroke="currentColor"
                   viewBox="0 0 24 24"
                 >
-<<<<<<< HEAD
-                  <path
-                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                  />
-                </svg>
-              </button>
-            </li>
-          </ul>
-          <div v-show="showAdditionalAttributes" class="mt-2" ref="attributes">
-            <div v-for="attribute in attributes" :key="attribute.name" class="mb-2">
-              <label class="text-gray-200 uppercase">{{ attribute.name.replace(/[\W_]+/g, " ") }}</label>
-              <select
-                v-if="attribute.list.length > 0"
-                class="block w-full h-10 bg-gray-200 focus:outline-none focus:bg-white"
-                :value="attributeValues[attribute.name]"
-                @change="e => setAttributeValue(e.target.value, attribute.name)"
-                @keydown.enter.prevent="focusNextAttributeField(attribute.name)"
-                @keydown.esc="close"
-              >
-                <option v-for="item in attribute.list" :value="item" :key="item">{{ item }}</option>
-              </select>
-              <input
-                v-if="attribute.list.length === 0"
-                type="text"
-                :value="attributeValues[attribute]"
-                class="w-full h-10 p-2 bg-gray-200 rounded focus:outline-none focus:bg-white"
-                @input="e => setAttributeValue(e.target.value, attribute.name)"
-                @keydown.enter.prevent="focusNextAttributeField(attribute)"
-                @keydown.esc="close"
-              />
-            </div>
-            <button
-              ref="addWithAttributeButton"
-              class="w-full h-10 mt-2 font-bold text-teal-800 uppercase bg-teal-400 rounded focus:outline-none focus:bg-teal-300"
-              @keydown.tab.prevent="$refs.close.focus"
-              @click.prevent="onSubmitAddWithAttributes"
-              @keydown.enter.prevent="onSubmitAddWithAttributes"
-              @keydown.esc="close"
-            >Add</button>
-          </div>
-          <div
-            class="w-full px-2 py-2 font-semibold text-left text-gray-500"
-            v-show="!filteredOptions.length && !showAdditionalAttributes"
-          >
-            {{
-            options.length === 0 ? "No option available" : "No result found"
-            }}
-=======
                   <path d="M6 18L18 6M6 6l12 12"></path>
                 </svg>
               </button>
@@ -478,7 +293,6 @@
                 options.length === 0 ? "No option available" : "No result found"
               }}
             </div>
->>>>>>> cf40c88a78ea0db474be0fe4898e6ebb48e49e19
           </div>
         </div>
       </div>

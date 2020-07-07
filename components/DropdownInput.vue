0d61d038751cf9b94eef1893ec6b6d35e082b557
Sebastien Koziel <sebastien@sbkl.ltd>
2020-07-07 14:42:05 +0800

    dropdown input disabled button

--- components/DropdownInput.vue
+++ components/DropdownInput.vue
@@ -12,21 +12,14 @@
             ref="button"
             type="button"
             @click="open"
-            class="inline-flex justify-between w-full px-4 py-2 text-xs font-medium leading-5 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 active:bg-gray-50 active:text-gray-800"
-            :class="[
-              {
+            :disabled="disabled"
+            class="inline-flex justify-between w-full px-4 py-2 text-xs font-medium leading-5 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md"
+            :class="[disabled ? 'cursor-not-allowed': 'hover:text-gray-500 active:bg-gray-50 active:text-gray-800 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue', value === '' || disabled ? 'text-gray-400' : 'text-gray-700',               {
                 'border-red-500':
                   formType === failedFormName && errors[errorAttribute],
-              },
-              disabled
-                ? 'cursor-not-allowed text-gray-500'
-                : 'text-gray-700 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue',
-            ]"
+              }]"
           >
-            <span
-              class="truncate"
-              :class="value === '' ? 'text-gray-400' : 'text-gray-700'"
-            >{{ value != "" ? value : placeholder }}</span>
+            <span class="truncate">{{ value != "" ? value : placeholder }}</span>
             <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20">
               <path
                 fill-rule="evenodd"

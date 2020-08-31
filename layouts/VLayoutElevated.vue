735248f346c3d5e4f2b8a29023a8d356901600b9
Sebastien Koziel <sebastien@sbkl.ltd>
2020-08-31 13:44:39 +0800

    added title to elevated navigation

--- layouts/VLayoutElevated.vue
+++ layouts/VLayoutElevated.vue
@@ -1,33 +1,33 @@
 <template>
   <div>
-    <div class="bg-gray-800 pb-32">
+    <div class="pb-32 bg-gray-800">
       <nav class>
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div class="border-b border-gray-700">
             <div class="flex items-center justify-between h-16 px-4 sm:px-0">
               <div class="flex items-center">
-                <div class="flex-shrink-0">
+                <div class="flex items-center flex-shrink-0">
                   <nuxt-link :to="{ name: 'index' }">
                     <img :src="logoImage" class="w-10 h-10 rounded-lg" />
                   </nuxt-link>
+                  <span v-if="title" class="ml-4 text-white">{{ title }}</span>
                 </div>
                 <div class="hidden md:block">
-                  <div class="ml-10 flex items-baseline">
+                  <div class="flex items-baseline ml-10">
                     <template v-for="([title, page, auth], index) in routes">
                       <nuxt-link
                         v-if="auth"
                         :key="page"
                         :to="{ name: page }"
-                        class="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+                        class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
                         :class="{ 'ml-4': index != 0 }"
-                        >{{ title }}</nuxt-link
-                      >
+                      >{{ title }}</nuxt-link>
                     </template>
                   </div>
                 </div>
               </div>
               <div class="hidden md:block">
-                <div class="ml-4 flex items-center md:ml-6">
+                <div class="flex items-center ml-4 md:ml-6">
                   <template
                     v-for="([action, authorised, callbacks = null],
                     index) in actions"
@@ -41,17 +41,12 @@
                   </template>
                 </div>
               </div>
-              <div class="-mr-2 flex md:hidden">
+              <div class="flex -mr-2 md:hidden">
                 <button
                   @click="open = !open"
-                  class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
+                  class="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
                 >
-                  <svg
-                    class="h-6 w-6"
-                    stroke="currentColor"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
+                  <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                     <path
                       class="inline-flex"
                       stroke-linecap="round"
@@ -73,25 +68,19 @@
             </div>
           </div>
         </div>
-        <div
-          class="border-b border-gray-700 md:hidden"
-          :class="open ? 'block' : 'hidden'"
-        >
+        <div class="border-b border-gray-700 md:hidden" :class="open ? 'block' : 'hidden'">
           <div class="px-2 py-3 sm:px-3">
             <nuxt-link
               v-for="([title, page], index) in routes"
               :key="page"
               :to="{ name: page }"
-              class="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+              class="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
               :class="{ 'mt-1': index != 0 }"
               @click.native="open = false"
-              >{{ title }}</nuxt-link
-            >
+            >{{ title }}</nuxt-link>
           </div>
           <div class="px-2 py-3 sm:px-3">
-            <template
-              v-for="([action, authorised, callbacks = null], index) in actions"
-            >
+            <template v-for="([action, authorised, callbacks = null], index) in actions">
               <component
                 :key="index"
                 :is="action"
@@ -104,13 +93,13 @@
         </div>
       </nav>
       <header class="py-12">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
           <portal to="header" />
         </div>
       </header>
     </div>
     <main class="-mt-32">
-      <div class="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
+      <div class="px-4 pb-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <slot></slot>
       </div>
     </main>
@@ -119,11 +108,11 @@
 <script>
 export default {
   name: "VLayoutElevated",
-  props: ["logoImage", "routes", "actions"],
+  props: ["logoImage", "routes", "actions", "title"],
   data() {
     return {
-      open: false
+      open: false,
     };
-  }
+  },
 };
 </script>

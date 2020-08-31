a0b06177141284ac4dd433d39f88aef023551bba
Sebastien Koziel <sebastien@sbkl.ltd>
2020-08-31 13:58:48 +0800

    fixed order notification panel

--- components/VNotificationPanel.vue
+++ components/VNotificationPanel.vue
@@ -3,19 +3,19 @@
     <transition name="fade" appear>
       <div
         @click.prevent="hideNotificationPanel"
-        class="absolute inset-0 bg-black-opacity-50 z-20"
+        class="absolute inset-0 z-20 bg-black-opacity-50"
       />
     </transition>
     <transition name="slide" appear>
-      <div class="absolute bg-white inset-y-0 w-full sm:w-96 right-0 z-30">
+      <div class="absolute inset-y-0 right-0 z-30 w-full bg-white sm:w-96">
         <div class="flex items-center justify-between">
-          <h1 class="text-3xl font-bold pl-4 py-4">Notifications</h1>
+          <h1 class="py-4 pl-4 text-3xl font-bold">Notifications</h1>
           <button
             @click.prevent="hideNotificationPanel"
-            class="flex items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-gray-600"
+            class="flex items-center justify-center w-12 h-12 rounded-full focus:outline-none focus:bg-gray-600"
           >
             <svg
-              class="h-6 w-6 text-gray-400"
+              class="w-6 h-6 text-gray-400"
               stroke="currentColor"
               fill="none"
               viewBox="0 0 24 24"
@@ -31,12 +31,15 @@
         </div>
         <div class="overflow-scroll">
           <div
-            class="border-b border-gray-200 p-2"
+            class="p-2 border-b border-gray-200"
             v-for="notification in notifications"
             :key="notification.id"
           >
-            <p class="text-gray-800">{{ notification.data.message }}</p>
-            <p class="text-gray-500 text-sm">{{ notification.created_at }}</p>
+            <p class="text-gray-800">
+              Order #{{ notification.data.order_number }} is now
+              {{ notification.data.status }}
+            </p>
+            <p class="text-sm text-gray-500">{{ notification.created_at }}</p>
           </div>
         </div>
       </div>
@@ -50,8 +53,8 @@ export default {
   props: ["showNotificationPanel"],
   computed: {
     ...mapGetters({
-      notifications: "notifications/notifications"
-    })
+      notifications: "notifications/notifications",
+    }),
   },
   mounted() {
     this.fetchNotifications();
@@ -59,13 +62,13 @@ export default {
   methods: {
     ...mapActions({
       fetchNotifications: "notifications/index",
-      markNotificationsAsRead: "notifications/markNotificationsAsRead"
+      markNotificationsAsRead: "notifications/markNotificationsAsRead",
     }),
     hideNotificationPanel() {
       this.markNotificationsAsRead();
       this.$emit("hideNotificationPanel");
-    }
-  }
+    },
+  },
 };
 </script>
 <style scoped>

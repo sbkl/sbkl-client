8e14977ab92c7996293464cc4d91a2d57dccb0b3
sbkl <sebastien@sbkl.ltd>
2021-02-06 18:32:54 +0800

    Input and Modal fixes

--- components/Input.vue
+++ components/Input.vue
@@ -1,31 +1,59 @@
 <template>
   <div>
-    <label :for="attribute" class="block text-sm font-medium text-gray-700">{{ labelText }}</label>
+    <label :for="attribute" class="block text-sm font-medium text-gray-700">{{
+      labelText
+    }}</label>
     <div class="relative mt-1 rounded-md shadow-sm">
-      <input 
-        :type="type" 
+      <input
+        ref="input"
+        :disabled="disabled"
+        :type="type"
         :step="step"
-        :name="attribute" 
-        :id="attribute" 
-        class="block w-full rounded-md focus:outline-none sm:text-sm"        
-        :class="formType === failedFormName && errors[attribute] ? 'pr-10 text-red-900 placeholder-red-300 border-red-300 focus:ring-red-500 focus:border-red-500 ' : 'shadow-sm focus:ring-gray-500 focus:border-gray-500 border-gray-300'"
+        :name="attribute"
+        :id="attribute"
+        class="block w-full rounded-md focus:outline-none sm:text-sm"
+        :class="[
+          formType === failedFormName && errors[attribute]
+            ? 'pr-10 text-red-900 placeholder-red-300 border-red-300 focus:ring-red-500 focus:border-red-500 '
+            : 'shadow-sm focus:ring-gray-500 focus:border-gray-500 border-gray-300',
+          { 'text-gray-500 cursor-not-allowed': disabled },
+        ]"
         :value="value"
         @input="inputChange"
-        aria-invalid="true" 
-        :aria-describedby="`${attribute}-error`">
-      <div v-if="formType === failedFormName && errors[attribute]" class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+        aria-invalid="true"
+        :aria-describedby="`${attribute}-error`"
+      />
+      <div
+        v-if="formType === failedFormName && errors[attribute]"
+        class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none"
+      >
         <!-- Heroicon name: exclamation-circle -->
-        <svg class="w-5 h-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-          <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+        <svg
+          class="w-5 h-5 text-red-500"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+            clip-rule="evenodd"
+          />
         </svg>
       </div>
     </div>
-    <p       v-if="
+    <p
+      v-if="
         formType === failedFormName &&
-          errors[attribute] &&
-          !Array.isArray(errors[attribute])
+        errors[attribute] &&
+        !Array.isArray(errors[attribute])
       "
-      class="mt-2 text-sm text-red-600" id="email-error">{{ errors[attribute] }}</p>
+      class="mt-2 text-sm text-red-600"
+      id="email-error"
+    >
+      {{ errors[attribute] }}
+    </p>
   </div>
 </template>
 <script>
@@ -34,47 +62,53 @@ export default {
   props: {
     type: {
       type: String,
-      default: 'text'
+      default: "text",
     },
     step: {
       type: String,
-      default: '1'
-    },    
+      default: "1",
+    },
     label: {
       type: String,
-      default: ''
+      default: "",
     },
     attribute: {
       type: String,
-      default: ''
+      default: "",
     },
     value: {
       type: [Number, String],
-      default: ""
+      default: "",
     },
     disabled: {
       type: Boolean,
       default: false,
-    }
+    },
   },
   computed: {
     labelText() {
-      if(this.label) {
-        return this.label.replace(/_/g, " ")  
+      if (this.label) {
+        return this.label.replace(/_/g, " ");
       }
-      return this.attribute.replace(/_/g, " ").charAt(0).toUpperCase() + this.attribute.replace(/_/g, " ").substring(1)      
-    }
+      return (
+        this.attribute.replace(/_/g, " ").charAt(0).toUpperCase() +
+        this.attribute.replace(/_/g, " ").substring(1)
+      );
+    },
   },
-    data() {
+  data() {
     return {
-      formType: this.$parent.formType
+      formType: this.$parent.formType,
     };
   },
   methods: {
     inputChange(e) {
       this.clearFieldErrors(this.attribute);
       this.$emit("input", e.target.value);
-    }
-  }
-}
+    },
+    focus() {
+      this.$refs.input.focus();
+    },
+  },
+};
 </script>
--- components/VModal.vue
+++ components/VModal.vue
@@ -1,25 +1,50 @@
 <template>
   <div
     class="fixed inset-0 z-10 overflow-y-auto"
-    :class="{'hidden' : !containerShow}"
+    :class="{ hidden: !containerShow }"
   >
-  <div class="flex items-end justify-center min-h-screen text-center sm:block sm:p-0">
-    <opacity-transition>
-      <div v-if="isOpen" class="fixed inset-0 transition-opacity" aria-hidden="true">
-        <div @click.self="closeModal" class="absolute inset-0 bg-gray-500 opacity-75"></div>
-      </div>
-    </opacity-transition>
-    <!-- This element is to trick the browser into centering the modal contents. -->
-    <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-
-    <modal-transition>
-      <div v-if="isOpen" class="inline-block overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:align-middle sm:max-w-xl sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-        <div class="flex flex-col min-h-0 overflow-hidden text-left align-bottom sm:max-h-80vh sm:align-middle" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-        <slot></slot>
+    <div
+      class="flex items-end justify-center min-h-screen text-center sm:block sm:p-0"
+    >
+      <opacity-transition>
+        <div
+          v-if="isOpen"
+          class="fixed inset-0 transition-opacity"
+          aria-hidden="true"
+        >
+          <div
+            @click.self="closeModal"
+            class="absolute inset-0 bg-gray-500 opacity-75"
+          ></div>
         </div>
+      </opacity-transition>
+      <!-- This element is to trick the browser into centering the modal contents. -->
+      <span
+        class="hidden sm:inline-block sm:align-middle sm:h-screen"
+        aria-hidden="true"
+        >&#8203;</span
+      >
+
+      <modal-transition>
+        <div
+          v-if="isOpen"
+          class="inline-block overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:align-middle sm:max-w-xl"
+          role="dialog"
+          aria-modal="true"
+          aria-labelledby="modal-headline"
+        >
+          <div
+            class="flex flex-col min-h-0 overflow-hidden text-left align-bottom sm:max-h-80vh sm:align-middle"
+            :class="modalContainerStyle"
+            role="dialog"
+            aria-modal="true"
+            aria-labelledby="modal-headline"
+          >
+            <slot></slot>
+          </div>
+        </div>
+      </modal-transition>
     </div>
-    </modal-transition>
-  </div>
   </div>
 </template>
 <script>
@@ -27,36 +52,36 @@ export default {
   name: "VModal",
   props: {
     open: {
-      default: null
+      default: null,
     },
     modalContainerStyle: {
       type: String,
-      default: "w-full lg:w-1/2"
-    }
+      default: "w-full lg:w-1/2",
+    },
   },
   data() {
     return {
       isOpen: false,
-      containerShow: false
+      containerShow: false,
     };
   },
   watch: {
     open() {
       this.isOpen = !this.isOpen;
-      if(this.isOpen) {
+      if (this.isOpen) {
         document.body.classList.add("overflow-hidden");
-        this.containerShow = true
+        this.containerShow = true;
       } else {
         setTimeout(() => {
-          this.containerShow = false
-        }, 400)
+          this.containerShow = false;
+        }, 400);
       }
       this.clearAllErrors();
-    }
+    },
   },
   created() {
     if (process.client) {
-      const closeModelOnEsc = e => {
+      const closeModelOnEsc = (e) => {
         if (e.keyCode === 27) {
           this.isOpen = false;
         }
@@ -72,7 +97,7 @@ export default {
     closeModal() {
       document.body.classList.remove("overflow-hidden");
       this.$parent.$emit("closeModal");
-    }
-  }
+    },
+  },
 };
 </script>
